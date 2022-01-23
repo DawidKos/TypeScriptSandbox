489b13f101b126b79a9cd5acfeb7f789960c3dcd
Dawid.S <dawid.baltica@gmail.com>
2022-01-24 00:21:21 +0100

    Initial commit

---
+++ dist/index.js
@@ -0,0 +1,13 @@
+let isNumber = 55;
+const isTrue = true;
+const isStringArray1 = [];
+const isStringArray2 = [];
+const isTuple = [2, 'a'];
+var Colors;
+(function (Colors) {
+    Colors["Red"] = "Czerwony";
+    Colors["Green"] = "Zielony";
+    Colors["Blue"] = "Niebieski";
+})(Colors || (Colors = {}));
+console.log(isNumber, isTrue, isStringArray1, isStringArray2, isTuple, Colors.Red, Colors.Green, Colors.Blue);
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,16 @@
+let isNumber: number = 55;
+const isTrue = true;
+const isStringArray1: Array<string> = [];
+const isStringArray2: string[] = [];
+const isTuple: [number, string] = [2, 'a'];
+enum Colors {
+    Red = "Czerwony",
+    Green = "Zielony",
+    Blue = "Niebieski"
+}
+
+
+
+console.log(isNumber, isTrue, isStringArray1, isStringArray2, isTuple, Colors.Red, Colors.Green, Colors.Blue
+
+);
